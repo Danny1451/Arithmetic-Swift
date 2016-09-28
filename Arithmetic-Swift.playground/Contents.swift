c595c5d15002f27ef10a6369069c08f25dbb34c0
Danny1451 <ricky1451@gmail.com>
2016-09-28 19:22:09 +0800

    init

---
+++ Arithmetic-Swift.playground/Contents.swift
@@ -0,0 +1,125 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Foundation
+
+var str = "Hello, playground"
+
+
+
+//0-100 桶排序
+func TongSort( numbers: [Int])->[Int]{
+    var tong : [Int] = [0]
+    var res : [Int] = Array()
+    for _ in 0...100 {
+        tong.append(0)
+    }
+    
+    for num in numbers {
+        tong[num] = 1
+    }
+    
+    for i in 0...100 {
+        if tong[i] == 1 {
+            res.append(i)
+        }
+    }
+    
+    return res
+    
+}
+
+//冒泡排序
+func PaoSort( numbers: inout [Int]){
+    
+    
+    for i in 0..<(numbers.count) {
+        
+        for j in 0..<(numbers.count-1-i) {
+            
+            
+            if numbers[j] > numbers[j+1] {
+                let temp = numbers[j]
+                
+                numbers[j] = numbers[j+1]
+                
+                numbers[j+1] = temp
+            }
+        }
+        
+    }
+    
+}
+
+//快速排序
+func FastSort( numbers: inout [Int] , start : Int , end : Int){
+    
+    if start < end {
+        
+        
+        let temp = numbers[start]
+        var i = start
+        var j = end
+        
+        while i != j {
+            
+            while(numbers[j] >= temp ) && (i < j) {
+                
+                j = j - 1
+                
+            }
+            
+            
+            while(numbers[i] <= temp ) && (i < j) {
+                
+                i = i + 1
+                
+            }
+            
+            
+            if i < j {
+                let temp = numbers[i]
+                
+                numbers[i] = numbers[j]
+                
+                numbers[j] = temp
+            }
+            
+
+        }
+        
+        numbers[start] = numbers[i]
+        numbers[i] = temp
+        
+        FastSort(numbers: &numbers , start: start, end: i-1)
+        FastSort(numbers: &numbers , start: i + 1, end: end)
+    }
+    
+}
+
+func timeNow()->Double{
+    let timeIntervalTongS:TimeInterval = NSDate().timeIntervalSince1970
+    let timeStampTongS = Double(timeIntervalTongS)
+    return timeStampTongS;
+}
+//当前时间的时间戳
+let t1 = timeNow()
+TongSort(numbers: [13,41,5,56,2,45,67,21,30,3])
+let t2 = timeNow()
+let tongTime = t2 - t1
+
+
+let t3 = timeNow()
+var arrayPao = [13,41,5,56,2,45,67,21,30,3]
+PaoSort(numbers:  &arrayPao)
+let t4 = timeNow()
+let paoTime = t4 - t3
+
+let t5 = timeNow()
+var array = [13,41,5,56,2,45,67,21,30,3]
+FastSort(numbers: &array ,start: 0, end:(array.count - 1) )
+let t6 = timeNow()
+let fastTime = t6 - t5
+
+
+
